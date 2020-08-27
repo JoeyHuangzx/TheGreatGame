4b160bf2e34705c257f9b0a96f852420fc0746a8
joeyhuang <joeyhuang312@gmail.com>
2020-08-27 18:11:37 +0800

    0

--- Assets/Scripts/Utility/FileUtility/ExcelHelper.cs
+++ Assets/Scripts/Utility/FileUtility/ExcelHelper.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.IO;
-using Excel;
 using System.Data;
 
 public class ExcelHelper 
@@ -10,17 +9,17 @@ public class ExcelHelper
     public static void ReadExcelFromRes(string _path)
     {
         FileStream stream = File.Open(Application.dataPath + "/Resources/Asteroid.xlsx",FileMode.Open,FileAccess.Read);
-        IExcelDataReader dataReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
-        DataSet result = dataReader.AsDataSet();
-        int columns = result.Tables[0].Columns.Count;
-        int rows = result.Tables[0].Rows.Count;
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns; j++)
-            {
-                Debug.Log(result.Tables[0].Rows[i][j].ToString());
-            }
-        }
+        //IExcelDataReader dataReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        //DataSet result = dataReader.AsDataSet();
+        //int columns = result.Tables[0].Columns.Count;
+        //int rows = result.Tables[0].Rows.Count;
+        //for (int i = 0; i < rows; i++)
+        //{
+        //    for (int j = 0; j < columns; j++)
+        //    {
+        //        Debug.Log(result.Tables[0].Rows[i][j].ToString());
+        //    }
+        //}
     }
     
 }
